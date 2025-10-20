6083f19ceb9ff548205a920b5ffe5ef13636e093
Harikrishna Natrajan <hari@tech101.in>
2025-10-20 14:24:56 +0530

    Contract for a flow that searches for data for based on a query

---
+++ domain/boundaries/query-flow/src/sbilifeco/flows/product_analyst/query_flow.py
@@ -0,0 +1,15 @@
+from typing import Protocol
+from sbilifeco.models.base import Response
+from pydantic import BaseModel
+
+
+class RatedAnswer(BaseModel):
+    answer: str
+    rating: float
+
+
+class IQueryFlow(Protocol):
+    async def search(self, query: str) -> Response[list[RatedAnswer]]:
+        """Perform a search based on the provided query and return a list of rated answers."""
+        """The list is sorted by rating in descending order."""
+        ...
