3e3860ece8b7224e2de94c5938b222237799dcd1
Harikrishna Natrajan <hari@tech101.in>
2025-10-20 15:07:07 +0530

    Contract method to generate a request ID before diving into the actual operation

--- domain/boundaries/ingest-flow/src/sbilifeco/boundaries/product_analyst/ingest_flow.py
+++ domain/boundaries/ingest-flow/src/sbilifeco/boundaries/product_analyst/ingest_flow.py
@@ -4,10 +4,34 @@ from io import BufferedIOBase, RawIOBase, TextIOBase
 
 
 class IngestFlow(Protocol):
+    async def request_ingestion(self) -> Response[str]:
+        """Request an ingestion operation."""
+        """Returns: a response containing a service generated request ID"""
+        ...
+
     async def ingest(
-        self, source: str | bytes | bytearray | TextIOBase | BufferedIOBase | RawIOBase
-    ) -> Response[None]: ...
+        self,
+        ingestion_request: str,
+        source: str | bytes | bytearray | TextIOBase | BufferedIOBase | RawIOBase,
+    ) -> Response[None]:
+        """Perform an ingestion operation."""
+        """Args:
+            ingestion_request: the request ID obtained from `request_ingestion`
+            source: the data source to be ingested
+        Returns: a response indicating success or failure of the ingestion"""
+        ...
 
 
 class IngestFlowListener(Protocol):
-    async def on_ingest(self, response: Response[None]) -> None: ...
+    async def on_request_ingestion(self, response: Response[str]) -> None:
+        """Handle the event that a request for ingestion has been made."""
+        """Args:
+            response: the response containing the request ID or error information"""
+        ...
+
+    async def on_ingest(self, ingestion_request: str, response: Response[None]) -> None:
+        """Handle the event that an ingestion operation has been performed."""
+        """Args:
+            ingestion_request: the request ID obtained from `request_ingestion`
+            response: the response indicating success or failure of the ingestion"""
+        ...
--- domain/boundaries/query-flow/src/sbilifeco/boundaries/product_analyst/query_flow.py
+++ domain/boundaries/query-flow/src/sbilifeco/boundaries/product_analyst/query_flow.py
@@ -9,11 +9,33 @@ class RatedAnswer(BaseModel):
 
 
 class IQueryFlow(Protocol):
-    async def search(self, query: str) -> Response[list[RatedAnswer]]:
+    async def request_search(self) -> Response[str]:
+        """Request a search request ID for performing queries."""
+        ...
+
+    async def search(
+        self, search_request_id: str, query: str
+    ) -> Response[list[RatedAnswer]]:
         """Perform a search based on the provided query and return a list of rated answers."""
-        """The list is sorted by rating in descending order."""
+        """
+        Args:
+            search_request_id: the ID used to identify the search request
+            query: the search query string
+        Returns:
+            A Response object containing a list of RatedAnswer or error information. The list is sorted by rating in descending order.
+        """
         ...
 
 
 class QueryFlowListener(Protocol):
-    async def on_search(self, response: Response[list[RatedAnswer]]) -> None: ...
+    async def on_request_search(self, response: Response[str]) -> None:
+        """Handle the event that a search request ID has been obtained."""
+        """Args:
+            response: the response containing the search request ID or error information"""
+        ...
+
+    async def on_search(self, response: Response[list[RatedAnswer]]) -> None:
+        """Handle the event that a search has been performed."""
+        """Args:
+            response: the response containing the list of rated answers or error information"""
+        ...
