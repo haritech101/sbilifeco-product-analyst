b945ba51ca9c062ef1a36c967f18e56e95823092
Harikrishna Natrajan <hari@tech101.in>
2025-10-20 15:31:54 +0530

    Contract class to read a given material and deliver chunks of it when asked for

---
+++ domain/boundaries/material-reader/src/sbilifeco/boundaries/product_analyst/material_reader.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+from io import BufferedIOBase, RawIOBase, TextIOBase
+from typing import Protocol
+from sbilifeco.models.base import Response
+
+
+class BaseMaterialReader:
+    def __init__(self) -> None:
+        self.listeners: list[IMaterialReaderListener] = []
+
+    def add_listener(self, listener: IMaterialReaderListener) -> None:
+        """Adds a listener to the Material Reader."""
+        self.listeners.append(listener)
+
+    async def read_material(
+        self,
+        material: str | bytes | bytearray | RawIOBase | BufferedIOBase | TextIOBase,
+    ) -> Response[str]:
+        """Reads material and returns a Response containing an ID that can be used to fetch the material later."""
+        ...
+
+    async def read_next_chunk(
+        self, material_id: str
+    ) -> Response[str | bytes | bytearray]:
+        """Reads the next chunk of material and returns a Response containing the chunk data."""
+        ...
+
+
+class IMaterialReaderListener(Protocol):
+    async def on_material_read(
+        self,
+        response: Response[str],
+    ) -> None:
+        """Called when material has been read."""
+        """
+        Args:
+            response (Response[str]): The response containing the material ID. This ID can be used to fetch the material later.
+        """
+        ...
+
+    async def on_chunk_read(
+        self,
+        response: Response[str | bytes | bytearray],
+    ) -> None:
+        """Called when a chunk of material has been read."""
+        """
+        Args:
+            response (Response[str | bytes | bytearray]): The response containing the chunk data.
+        """
+        ...
