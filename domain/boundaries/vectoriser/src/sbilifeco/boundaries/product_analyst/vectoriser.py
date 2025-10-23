1bb0405754bc678cc97d9eed84f4735f22a03d17
Harikrishna Natrajan <hari@tech101.in>
2025-10-23 07:49:40 +0530

    Contract for building vectorisers to convert raw data into vectors

---
+++ domain/boundaries/vectoriser/src/sbilifeco/boundaries/product_analyst/vectoriser.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+from typing import Protocol
+from sbilifeco.models.base import Response
+
+
+class BaseVectoriser:
+    def __init__(self):
+        self.listeners: list[IVectoriserListener] = []
+
+    def add_listener(self, listener: IVectoriserListener) -> BaseVectoriser:
+        self.listeners.append(listener)
+        return self
+
+    async def vectorise(
+        self, request_id: str, material: str | bytes | bytearray
+    ) -> Response[list[float | int]]:
+        """
+        Vectorise the given material.
+        Args:
+            request_id (str): A request ID that uniquely identifies this request and is useful when using listeners.
+            material (str | bytes | bytearray): The material to vectorise.
+        """
+        raise NotImplementedError()
+
+
+class IVectoriserListener(Protocol):
+    async def on_vectorised(
+        self,
+        request_id: str,
+        response: Response[list[float | int]],
+    ) -> None:
+        """
+        Called when a material has been vectorised.
+        Args:
+            request_id (str): The request ID for which this result is relevant.
+            response (Response[list[float | int]]): The response that contains the vectorised material.
+        """
+        ...
