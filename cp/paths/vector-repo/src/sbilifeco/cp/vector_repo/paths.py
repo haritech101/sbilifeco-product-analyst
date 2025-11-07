b93a8ccd17bf3217dc8a046d6501841f60696ab9
Harikrishna Natrajan <hari@tech101.in>
2025-11-07 08:41:12 +0530

    Seperate paths for search and delete by criteria

--- cp/paths/vector-repo/src/sbilifeco/cp/vector_repo/paths.py
+++ cp/paths/vector-repo/src/sbilifeco/cp/vector_repo/paths.py
@@ -1,5 +1,6 @@
 class VectorRepoPaths:
     BASE = "/api/v1/vectorised-data"  # POST
     BY_ID = BASE + "/{id}"  # GET, DELETE
-    BY_CRITERIA = BASE + "/search/by-criteria"  # POST due to payload
+    SEARCH_BY_CRITERIA = BASE + "/search/by-criteria"  # POST due to payload
+    DELETE_BY_CRITERIA = BASE + "/delete/by-criteria"  # POST due to payload
     BY_VECTOR = BASE + "/search/by-vector"  # POST due to payload
