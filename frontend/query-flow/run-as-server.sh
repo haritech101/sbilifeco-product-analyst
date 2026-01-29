05ff8657b5dc0170bd1928dd6c8f86079013140c
Harikrishna Natrajan <hari@tech101.in>
2026-01-29 10:46:49 +0530

    Adding npx command to launch live-server during development

--- frontend/query-flow/run-as-server.sh
+++ frontend/query-flow/run-as-server.sh
@@ -5,4 +5,4 @@ export $(grep -v '^#' .env | xargs)
 
 envsubst '$API_BASE_URL $MATERIAL_QUERIES_PATH' < product-query-ui/js/query-page.ts.template > product-query-ui/js/query-page.ts
 sleep 1
-live-server --port=8000
+npx live-server --port=8000
