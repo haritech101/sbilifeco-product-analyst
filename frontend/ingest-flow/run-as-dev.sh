5111dce3cac8c03f34e1b87d24bdb4d6f4cf0fa0
Harikrishna Natrajan <hari@tech101.in>
2026-01-30 12:52:36 +0530

    Open the browser after starting the live server

--- frontend/ingest-flow/run-as-dev.sh
+++ frontend/ingest-flow/run-as-dev.sh
@@ -6,4 +6,4 @@ export $(grep -v '^#' .env | xargs)
 envsubst '${HTTP_PORT} ${API_BASE_URL} ${API_INGEST_PATH} ${API_MATERIALS_PATH}' < ingest-ui/js/ingest-page.ts.template > /tmp/Xxx.ts
 mv /tmp/Xxx.ts ingest-ui/js/ingest-page.ts
 sleep 1
-npx live-server --port=${HTTP_PORT}
+npx live-server --port=${HTTP_PORT} --browser=google-chrome
