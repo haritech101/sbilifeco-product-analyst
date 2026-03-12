abe7d37041d4fdac664f1858cd74eea7ff3a954e
Harikrishna Natrajan <hari@tech101.in>
2026-03-12 10:28:12 +0530

    HTTP-based entrypoints for material reader microservice

--- cp/paths/material-reader/src/sbilifeco/cp/material_reader/paths.py
+++ cp/paths/material-reader/src/sbilifeco/cp/material_reader/paths.py
@@ -2,3 +2,4 @@ class MaterialReaderPaths:
     BASE = "/api/v1/materials"
     MATERIAL_BY_ID = BASE + "/{material_id}"
     NEXT_CHUNK = MATERIAL_BY_ID + "/next-chunk"
+    STREAMS = "/api/v1/material-streams"
